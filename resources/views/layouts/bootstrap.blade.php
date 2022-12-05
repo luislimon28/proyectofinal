8a90e4484b3389e50934788dfa0e0f2957d1277c
luislimon28 <89094516+luislimon28@users.noreply.github.com>
2022-12-04 19:49:04 -0600

    Update bootstrap.blade.php

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -311,7 +311,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; ESwim 2021</span>
+                        <span>Copyright &copy; ESwim 2022</span>
                     </div>
                 </div>
             </footer>
